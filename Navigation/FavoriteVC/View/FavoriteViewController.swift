26d226c09d56e6236ac4acddba77e4095159a9a6
zalik198 <zshomakhov@mail.ru>
2022-08-09 15:29:38 +0300

    deleting

--- Navigation/FavoriteVC/View/FavoriteViewController.swift
+++ Navigation/FavoriteVC/View/FavoriteViewController.swift
@@ -15,6 +15,59 @@ class FavoriteViewController: UIViewController {
     private weak var coordinator: FavoriteCoordinator?
     private var post: PostData?
     private let myInspector = Factory.shared.myFactory()
+    private var textFieldBottomConstraint: NSLayoutConstraint?
+    public var favoritePost = [PostData]()
+
+    
+    lazy var textFieldPanel: UIView = {
+        let view = UIView(frame: CGRect(x: 0.0, y: self.view.bounds.height, width: self.view.bounds.width, height: 75))
+        self.view.addSubview(view)
+        view.backgroundColor = UIColor.lightGray
+        view.alpha = 0.75
+        
+        let fieldBottomConstraint = NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1.0, constant: 0.0)
+        textFieldBottomConstraint = fieldBottomConstraint
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addConstraint(.init(item: view, attribute: .left, relatedBy: .equal, toItem: self.view, attribute: .left, multiplier: 1.0, constant: 0.0))
+        self.view.addConstraint(.init(item: view, attribute: .right, relatedBy: .equal, toItem: self.view, attribute: .right, multiplier: 1.0, constant: 0.0))
+        self.view.addConstraint(fieldBottomConstraint)
+        self.view.addConstraint(.init(item: view, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 75))
+        
+        return view
+    }()
+    
+    lazy var textField: UITextField = {
+        let field = UITextField(frame: CGRect(x: 0.0, y: 0.0, width: self.view.bounds.width, height: 50.0))
+        textFieldPanel.addSubview(field)
+        
+        //field.backgroundColor = .lightGray
+        field.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addConstraint(.init(item: field, attribute: .left, relatedBy: .equal, toItem: textFieldPanel, attribute: .left, multiplier: 1.0, constant: 8.0))
+        self.view.addConstraint(.init(item: field, attribute: .right, relatedBy: .equal, toItem: textFieldPanel, attribute: .right, multiplier: 1.0, constant: -8.0))
+        self.view.addConstraint(.init(item: field, attribute: .top, relatedBy: .equal, toItem: textFieldPanel, attribute: .top, multiplier: 1.0, constant: 0.0))
+        self.view.addConstraint(.init(item: field, attribute: .bottom, relatedBy: .equal, toItem: textFieldPanel, attribute: .bottom, multiplier: 1.0, constant: -50.0))
+        
+        return field
+    }()
+    
+    lazy var buttonInTextField: UIButton = {
+       let buttonInTextField = UIButton(frame: CGRect(x: 0, y: 0, width: 25, height: 25.0))
+       textFieldPanel.addSubview(buttonInTextField)
+
+        buttonInTextField.setTitle("Применить", for: .normal)
+        buttonInTextField.backgroundColor = .systemBlue
+        buttonInTextField.layer.cornerRadius = 10
+        
+        buttonInTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addConstraint(.init(item: buttonInTextField, attribute: .left, relatedBy: .equal, toItem: textFieldPanel, attribute: .left, multiplier: 1.0, constant: 40.0))
+       self.view.addConstraint(.init(item: buttonInTextField, attribute: .right, relatedBy: .equal, toItem: textFieldPanel, attribute: .right, multiplier: 1.0, constant: -40.0))
+        self.view.addConstraint(.init(item: buttonInTextField, attribute: .bottom, relatedBy: .equal, toItem: textFieldPanel, attribute: .bottom, multiplier: 1.0, constant: -3.0))
+        
+        
+        return buttonInTextField
+    }()
     
     //MARK: Initial tableView
     private var tableView: UITableView = {
@@ -28,7 +81,7 @@ class FavoriteViewController: UIViewController {
         //tableView.rowHeight = UITableView.automaticDimension
         return tableView
     }()
-
+    
     
     init(model: CoreDataManager, coordinator: FavoriteCoordinator ) {
         self.viewModel = model
@@ -48,14 +101,18 @@ class FavoriteViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         initialLayout()
-        tableView.register(FavoriteTableViewCell.self, forCellReuseIdentifier: "favoriteTableViewCell")
 
+        tableView.register(FavoriteTableViewCell.self, forCellReuseIdentifier: "favoriteTableViewCell")
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(searchAction))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .stop, target: self, action: #selector(cancelFilteredSearchAction))
     }
     
     override func viewWillAppear(_ animated: Bool) {
         CoreDataManager.shared.outFromCoreData()
         tableView.reloadData()
+        
     }
     
     //MARK: Initial constraints
@@ -68,13 +125,61 @@ class FavoriteViewController: UIViewController {
                                     ])
     }
     
- 
+    @objc func searchAction() {
+        //textField.becomeFirstResponder()
+        //textFieldBottomConstraint?.constant = -360.0
+        //textFieldPanel.addSubview(buttonInTextField)
+        
+        let alertController = UIAlertController(title: "Search author post", message: "", preferredStyle: .alert)
+
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Enter author"
+        }
+
+        let searchAction = UIAlertAction(title: "Search", style: .default, handler: { alert -> Void in
+            let textField = alertController.textFields![0] as UITextField
+            //let secondTextField = alertController.textFields![1] as UITextField
+        })
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil )
+
+//        alertController.addTextField { (textField : UITextField!) -> Void in
+//            textField.placeholder = "Enter First Name"
+//        }
+
+        alertController.addAction(searchAction)
+        alertController.addAction(cancelAction)
+
+        self.present(alertController, animated: true, completion: nil)
+
+    }
+    
+    @objc func cancelFilteredSearchAction() {
+    }
+    
+    
 }
 
 extension FavoriteViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let post = CoreDataManager.shared.favoritePost[indexPath.row]
+        let delete = UIContextualAction(style: .destructive, title: "delete") { (action, view, completionHandler) in CoreDataManager.shared.removeFromCoreData()
+           
+            
+        }
+        
+        tableView.reloadData()
+        let swipeActionsConfig = UISwipeActionsConfiguration(actions: [delete])
+               swipeActionsConfig.performsFirstActionWithFullSwipe = true
+               
+               return swipeActionsConfig
     }
 }
 
@@ -90,6 +195,7 @@ extension FavoriteViewController: UITableViewDataSource {
         cell.myCells(post)
         return cell
     }
+    
     
     
 }
